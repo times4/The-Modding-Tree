16f24c439d62f9bc8c654eb5cf0ab97b0ae84a11
times4 <113521094+times4@users.noreply.github.com>
2024-05-28 10:56:00 +0930

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
+	name: "The Testing Tree",
+	id: "remember",
 	author: "nobody",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
